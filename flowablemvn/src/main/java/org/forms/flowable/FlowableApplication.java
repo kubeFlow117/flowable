bc792089eea00668f35b9f0e8201c14c20133ee3
coderrb656 <coderrb656@gmail.com>
2026-08-08 21:01:14 -0500

    chore: update

--- flowablemvn/src/main/java/org/forms/flowable/FlowableApplication.java
+++ flowablemvn/src/main/java/org/forms/flowable/FlowableApplication.java
@@ -1,7 +1,9 @@
 package org.forms.flowable;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class FlowableApplication {
---
+++ flowablemvn/src/main/java/org/forms/flowable/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package org.forms.flowable.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic(); // Enables basic HTTP authentication
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
